854ad52a69b66cb7f82215363905c37a3804e4af
magour <magour@MAGOUR-5CB0L.partnet.cisco.com>
2020-01-20 14:22:01 +0530

    Initial commit

---
+++ src/main/java/com/hcl/learn/DataStructureAlgorithmsApplication.java
@@ -0,0 +1,13 @@
+package com.hcl.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataStructureAlgorithmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataStructureAlgorithmsApplication.class, args);
+	}
+
+}
